f857bca735866b39e4f8dd90654c903b656d81cb
ogh826 <eys555@naver.com>
2024-08-12 11:35:58 +0900

    Main Commit

---
+++ ConsoleProject_08.12~08.16/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleProject_08._12_08._16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
